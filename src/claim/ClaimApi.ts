eed14c0a674239fa31892a513067d41a9ffdbff9
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2021-08-20 18:04:20 +0300

    chore(unl-js-api): UNL-153 add create claim method

--- src/claim/ClaimApi.ts
+++ src/claim/ClaimApi.ts
@@ -1,8 +1,14 @@
 import { BaseAPI } from '../common/BaseApi';
 import { UnlApiConfig } from '../models';
+import { ClaimRequest } from './models/ClaimRequest';
+import { Claim } from './models/Claim';
 
 export default class ClaimApi extends BaseAPI {
   constructor(configuration: UnlApiConfig) {
     super(configuration);
+  }
+
+  public create(claimRequest: ClaimRequest): Promise<Claim> {
+    return this.restClient.post<Claim>('claiming', {}, claimRequest);
   }
 }
