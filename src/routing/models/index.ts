6265861b28572a73b4cc6270d1a8e50032fa6531
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2021-08-20 18:07:40 +0300

    fix(unl-js-api): UNL-154 fix types export

---
+++ src/routing/models/index.ts
@@ -0,0 +1,7 @@
+export * from './CoordinatesWaypoint';
+export * from './GeohashWaypoint';
+export * from './IndoorWaypoint';
+export * from './Route';
+export * from './RouteOverview';
+export * from './RouteRequest';
+export * from './WaypointTypeEnum';
