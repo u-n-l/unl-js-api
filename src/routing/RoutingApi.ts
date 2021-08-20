f17aee9b68611274f4c2974c22abf0495709ce43
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2021-08-20 15:49:17 +0300

    chore(unl-js-api): UNL-142 implement route method

--- src/routing/RoutingApi.ts
+++ src/routing/RoutingApi.ts
@@ -1,8 +1,18 @@
 import { BaseAPI } from '../common/BaseApi';
 import { UnlApiConfig } from '../models';
+import { RouteRequest } from './models/RouteRequest';
+import { Route } from './models/Route';
 
 export default class RoutingApi extends BaseAPI {
   constructor(configuration: UnlApiConfig) {
     super(configuration);
+  }
+
+  public route(projectId: string, routeRequest: RouteRequest): Promise<Route> {
+    const pathParamMap = {
+      project_id: projectId,
+    };
+
+    return this.restClient.post<Route>('routing/{project_id}', pathParamMap, routeRequest);
   }
 }
