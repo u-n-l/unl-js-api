8a68b0cb26a1eda5667056082f64b4bc14763789
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2021-08-24 19:10:12 +0300

    chore(unl-js-api): UNL-145 add search method

--- src/search/SearchApi.ts
+++ src/search/SearchApi.ts
@@ -1,8 +1,13 @@
 import { BaseAPI } from '../common/BaseApi';
 import { UnlApiConfig } from '../models';
+import { SearchQueryParams } from './models/SearchQueryParams';
 
 export default class SearchApi extends BaseAPI {
   constructor(configuration: UnlApiConfig) {
     super(configuration);
+  }
+
+  public search(searchQueryParams: SearchQueryParams): Promise<GeoJSON.FeatureCollection> {
+    return this.restClient.get<GeoJSON.FeatureCollection>('search', {}, searchQueryParams);
   }
 }
