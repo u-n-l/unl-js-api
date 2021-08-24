3ac6b8afda1594c8027b9d60bffe9f5ff079f541
Bogdan Simon <Bogdan.Simon@rebeldot.com>
2021-08-24 19:27:31 +0300

    chore(unl-js-api): UNL-146 Add jsdoc. for SearchQueryParams

--- src/search/models/SearchQueryParams.ts
+++ src/search/models/SearchQueryParams.ts
@@ -1,6 +1,22 @@
+/**
+ * The type representing the query params used for search.
+ * @alias Record
+ */
 export type SearchQueryParams = {
+  /**
+   * Search input.
+   */
   q: string;
+  /**
+   * Latitude.
+   */
   lat: number;
+  /**
+   * Longitude.
+   */
   lon: number;
+  /**
+   * Optional: radius.
+   */
   radius?: number;
 };
